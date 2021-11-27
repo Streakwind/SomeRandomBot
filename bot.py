9bb460ca30356dc6f6496e7afc5aec4c797fa026
Streakwind <streakwindofficial@gmail.com>
2021-11-26 20:19:45 -0800

    Re-initializing the repository.

---
+++ bot.py
@@ -0,0 +1,84 @@
+import discord
+from discord.ext import commands  
+from discord import Member
+import traceback
+import sys
+import datetime
+import config
+
+intents = discord.Intents.default()
+intents.members = True
+description='''Hello, I'm a bot written with discord.py'''
+
+def get_prefix(bot, message):
+  return ["cc.", "cc! "]
+
+initial_extensions = (
+    'cogs.commands',
+    'cogs.moderation',
+    'cogs.music',
+)
+
+class CherryCrestBot (commands.Bot):
+    def __init__(self):
+        self.uptime=datetime.datetime.utcnow()
+        
+        super().__init__(
+            command_prefix=get_prefix,
+            description=description,
+            owner_id=714554283026153554,
+            intents=intents,
+        )
+        
+        self.load_extension("jishaku")
+        for extension in initial_extensions:
+            try:
+                self.load_extension(extension)
+            except Exception as e:
+                print(f'Failed to load extension {extension}.', file=sys.stderr)
+                traceback.print_exc()
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"Logged in as {bot.user.name}")
+        await self.change_presence(activity=discord.Game('cc.help'))
+        
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        """The event triggered when an error is raised while invoking a command.
+        Parameters
+        ------------
+        ctx: commands.Context
+            The context used for command invocation.
+        error: commands.CommandError
+            The Exception raised.
+        """
+        if hasattr(ctx.command, 'on_error'):
+            return
+
+        cog = ctx.cog
+        if cog:
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                return
+
+        #ignored = (commands.CommandNotFound, )
+        error = getattr(error, 'original', error)
+        
+        #if isinstance(error, ignored):
+         #   return
+        
+        embed = discord.Embed(title="Command Error", description=f"Ignoring exception in command {ctx.command}", color=discord.Color.blue())
+        embed.add_field(name="Error", value=f"`{str(error)}`")
+            
+        await ctx.send(embed=embed)
+    
+    @commands.Cog.listener('on_message')
+    async def _hello(self, message):
+        if message.content.startswith('hello'):
+            await message.channel.send('hello')
+    
+bot = CherryCrestBot()
+
+bot.help_command = commands.MinimalHelpCommand()
+
+bot.run(config.token)
---
+++ cogs/commands.py
@@ -0,0 +1,92 @@
+from discord.ext import commands
+import discord
+import traceback
+import datetime
+import humanize
+from humanize import precisedelta
+import sys
+import typing
+
+class Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def time_left(self, ctx):
+        date=datetime.datetime(2022, 9, 1, hour=8, minute=30, second=0)
+        humanized_version=precisedelta(date)
+    
+        await ctx.send(f"{humanized_version} left")
+    
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def reload(self, ctx, extension):
+        """Reload an extension"""
+        try:
+            self.bot.reload_extension(extension)
+            print(f"{extension} successfully reloaded.")
+            emoji = '\N{THUMBS UP SIGN}'
+            await ctx.message.add_reaction(emoji)
+        except Exception as e:
+            # this next line formats the traceback and sends it
+            error = "".join(traceback.format_exception(type(e), e, e.__traceback__, 1))
+            return await ctx.send(f"Failed to reload extension.\n```{error}```")
+    
+    @commands.command()
+    async def prefix(self, ctx):
+        """Prefixes for the bot"""
+        await ctx.send("Prefixes: `cc.`, `cc!`")
+    
+    @commands.command(aliases=["latency"])
+    async def ping(self, ctx):
+        """The bots ping/latency"""
+        
+        ping = self.bot.latency * 1000
+        
+        ping = int(ping)
+        await ctx.send(f"My ping is {ping}ms")
+    
+    @commands.command(aliases = ["ui"])
+    async def userinfo(self, ctx, *, member: discord.Member = None):
+        """Information about a certain user"""
+        
+        if not member:
+            member = ctx.author
+        
+        embed = discord.Embed(title="ALL TIMES ARE IN UTC", description="", color=discord.Color.blue())
+        embed.set_author(name=f"{member} - {member.id}", icon_url=member.avatar_url)
+        embed.set_thumbnail(url=member.avatar_url)
+         
+        time=precisedelta(member.created_at, minimum_unit="hours")
+        
+        embed.add_field(name="User created at", value=f"{time} ago", inline=True)
+        
+        if ctx.guild: 
+            if member in ctx.guild.members:
+                time_1=precisedelta(member.joined_at, minimum_unit="hours")
+                
+                embed.add_field(name="User joined at", value=f"{time_1} ago", inline=True)
+            else:
+                embed.description += f"This user ({member}) is not in the guild"
+        
+        if member.bot:
+            embed.description += "This user is a bot"
+            
+        if ctx.guild:     
+            if member.id == ctx.guild.owner.id:
+                embed.description += f"\nThis user owns this server ({ctx.guild.name})"
+        if member.id == self.bot.owner_id:
+            embed.description += "This user owns the bot"
+            
+        await ctx.send(embed = embed)
+    
+    @commands.command()
+    async def uptime(self, ctx):
+        uptime_before = datetime.datetime.utcnow() - self.bot.uptime
+        
+        uptime = precisedelta(uptime_before)
+        
+        await ctx.send(f"I booted up {uptime} ago")
+        
+def setup(bot):
+    bot.add_cog(Commands(bot))
